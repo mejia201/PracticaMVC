49d63878645db9fe04ec52c24d39591417998f6d
mejia201 <bryan.mejia@catolica.edu.sv>
2023-03-31 17:15:30 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+
+    }
+}
